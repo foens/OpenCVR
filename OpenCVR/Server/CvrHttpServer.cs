e7ea73393689655d3c3e55bf241645a53cc3d6eb
Kasper Fons <kfoens@gmail.com>
2015-05-26 22:05:16 +0200

    Fix

--- OpenCVR/Server/CvrHttpServer.cs
+++ OpenCVR/Server/CvrHttpServer.cs
@@ -73,7 +73,7 @@ namespace OpenCVR.Server
                     case "/api/1/search":
                         string search = context.Request.QueryString["q"];
                         var company = persistence.Search(search);
-                        streamWriter.Write($"{{VatNumber = {company.VatNumber}}}");
+                        streamWriter.Write(String.Format("{{VatNumber = {0}}}", company.VatNumber));
                         break;
                     default:
                         context.Response.StatusCode = (int) HttpStatusCode.NotFound;
